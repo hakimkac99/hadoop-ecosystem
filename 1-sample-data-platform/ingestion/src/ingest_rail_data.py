ffc22ba492e65133e1ef46ccce2a09489252fef1
hakimkac99 <kacemi.mohammed.abdelhakim@gmail.com>
2025-07-13 20:17:27 +0200

    fix linting

--- 1-sample-data-platform/ingestion/src/ingest_rail_data.py
+++ 1-sample-data-platform/ingestion/src/ingest_rail_data.py
@@ -1,15 +1,20 @@
-import requests
-from requests.auth import HTTPBasicAuth
-from datetime import datetime
-from hdfs import InsecureClient
 import os
+from datetime import datetime
 
+import requests
+from hdfs import InsecureClient
+from requests.auth import HTTPBasicAuth
 
-RAIL_DATA_DOWNLOAD_URL = "https://publicdatafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_ALL_FULL_DAILY&day=toc-full"
+RAIL_DATA_DOWNLOAD_URL = (
+    "https://publicdatafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate"
+    "?type=CIF_ALL_FULL_DAILY&day=toc-full"
+)
 HADOOP_NAMENODE_HOSTNAME = "http://hdfs-namenode:9870"
 
 # download data from the rail data API - https://wiki.openraildata.com/index.php?title=SCHEDULE
-basic_auth = HTTPBasicAuth(os.environ["RAIL_DATA_USERNAME"], os.environ["RAIL_DATA_PASSWORD"])
+basic_auth = HTTPBasicAuth(
+    os.environ["RAIL_DATA_USERNAME"], os.environ["RAIL_DATA_PASSWORD"]
+)
 results = requests.get(url=RAIL_DATA_DOWNLOAD_URL, auth=basic_auth, timeout=600)
 results.raise_for_status()
 
