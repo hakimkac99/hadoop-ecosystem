a56d3ec61bcb9a047635b8610985a3adcaf1a7dd
hakimkac99 <kacemi.mohammed.abdelhakim@gmail.com>
2025-08-16 19:50:43 +0200

    restructure Orchestration code && adding PySpark jobs submission helper

---
+++ 1-sample-data-platform/orchestration/prefect_flows/daily.py
@@ -0,0 +1,19 @@
+from models.pyspark_job import DeployMode, PySparkJob
+from prefect import flow, get_run_logger
+from prefect_flows.landing.schedules import ingest_schedules
+
+
+@flow(name="Daily integration")
+def run_daily():
+    logger = get_run_logger()
+    logger.info("Starting daily integration flow ...")
+
+    # ingest schedules from API and load to HDFS
+    ingest_schedules()
+
+    logger.info("Submitting PySpark job to the Hadoop cluster ...")
+    # transform schedules
+    PySparkJob(
+        deploy_mode=DeployMode.CLUSTER,
+        py_files=["transformation.zip", "transformation/main.py"],
+    ).submit()
---
+++ 1-sample-data-platform/orchestration/prefect_flows/models/pyspark_job.py
@@ -0,0 +1,45 @@
+from enum import Enum
+from typing import List
+
+from prefect_shell import ShellOperation
+
+
+class DeployMode(Enum):
+    """
+    Possible YARN deployment methods
+    """
+
+    CLUSTER = "cluster"
+    CLIENT = "client"
+
+
+class PySparkJob:
+    """
+    A helper class for submitting Pyspark jobs using the Prefect ShellOperation
+    """
+
+    def __init__(
+        self,
+        deploy_mode: DeployMode,
+        py_files: List[str],
+        working_dir: str = "spark_apps",
+    ):
+        self.deploy_mode = deploy_mode
+        self.py_files = py_files
+        self.working_dir = working_dir
+
+    def submit(self):
+        with ShellOperation(
+            commands=[
+                (
+                    "spark-submit --master yarn "
+                    f"--deploy-mode {self.deploy_mode.value} --py-files {' '.join(self.py_files)}"
+                ),
+            ],
+            working_dir=self.working_dir,
+        ) as submit_spark_job:
+            # trigger runs the process in the background
+            spark_job_process = submit_spark_job.trigger()
+
+            # wait for the process to finish
+            spark_job_process.wait_for_completion()
