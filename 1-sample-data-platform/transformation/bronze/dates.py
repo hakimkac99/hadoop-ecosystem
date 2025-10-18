353556714c9fb67d29a71098dab4aecf8dd1e8d1
hakimkac99 <kacemi.mohammed.abdelhakim@gmail.com>
2025-10-18 18:25:32 +0200

    Add date dimension

---
+++ 1-sample-data-platform/transformation/bronze/dates.py
@@ -0,0 +1,38 @@
+from typing import Dict, Optional
+
+from helpers.hdfs import HDFSClient
+from models.etl_table import ETLTable
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import current_timestamp
+
+
+class DateBronzeTable(ETLTable):
+    def __init__(self, spark: SparkSession, hdfs: HDFSClient):
+        super().__init__(
+            spark=spark,
+            hdfs=hdfs,
+            name="date",
+            storage_path="bronze/open_rail_data/date",
+            partition_columns=["spark_job_creation_timestamp"],
+        )
+
+    def extract_upstream(self, run_upstream: bool) -> Optional[Dict[str, DataFrame]]:
+        self.logger.info("Start extracting dates from HDFS csv static file ...")
+
+        # Extracting data from HDFS
+        hdfs_base_url = "hdfs://hdfs-namenode:9000/user/root"
+        df: DataFrame = self.spark.read.csv(
+            f"{hdfs_base_url}/landing/open_rail_data/static_files/dates.csv",
+            header=True,
+        )
+        return {
+            "dates": df,
+        }
+
+    def transform(self, upstream_dataframes) -> DataFrame:
+        dates_df = upstream_dataframes["dates"]
+
+        dates_table_df: DataFrame = dates_df.withColumn(
+            "spark_job_creation_timestamp", current_timestamp()
+        )
+        return dates_table_df
---
+++ 1-sample-data-platform/transformation/silver/dim_date.py
@@ -0,0 +1,60 @@
+from datetime import date
+from typing import Dict, Optional
+
+from bronze.dates import DateBronzeTable
+from helpers.hdfs import HDFSClient
+from helpers.scd1 import scd1_merge
+from models.etl_table import ETLTable
+from pyspark.sql import DataFrame, SparkSession, Window
+from pyspark.sql.functions import col, row_number
+
+
+class DimDateSilverTable(ETLTable):
+    def __init__(self, spark: SparkSession, hdfs: HDFSClient):
+        super().__init__(
+            spark=spark,
+            hdfs=hdfs,
+            name="dim_date",
+            storage_path="silver/open_rail_data/dim_date",
+            partition_columns=["spark_job_creation_timestamp"],
+            primary_keys=["date"],
+            table_write_mode="overwrite",
+        )
+
+    def extract_upstream(self, run_upstream: bool) -> Optional[Dict[str, DataFrame]]:
+        date_bronze_etl = DateBronzeTable(spark=self.spark, hdfs=self.hdfs)
+        if run_upstream:
+            date_bronze_etl.run_etl()
+
+        # Extracting data from HDFS
+        today = (date.today()).strftime("%Y-%m-%d")
+        bronze_df = date_bronze_etl.read(
+            partition_values={"spark_job_creation_timestamp": f"{today}%"}
+        )
+        return {
+            "bronze_date": bronze_df,
+        }
+
+    def transform(self, upstream_dataframes: Dict[str, DataFrame]) -> DataFrame:
+        # deduplicate bronze table
+        dedup_window = Window.orderBy(
+            col("spark_job_creation_timestamp").desc()
+        ).partitionBy(*self.primary_keys)
+
+        deduplicated_bronze_table_df = (
+            upstream_dataframes["bronze_date"]
+            .withColumn("row_number", row_number().over(dedup_window))
+            .filter(col("row_number") == 1)
+            .drop(col("row_number"))
+        )
+
+        existing_silver_table_df = self.read()
+
+        # apply scd1
+        scd1_result = scd1_merge(
+            df_source=deduplicated_bronze_table_df,
+            df_target=existing_silver_table_df,
+            primary_keys=self.primary_keys,
+        )
+
+        return scd1_result
